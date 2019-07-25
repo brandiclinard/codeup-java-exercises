a8c0932e738ee0d286063ec3e3d206419425fffa
Brandi <brandiclinard021911@gmail.com>
2019-07-25 16:33:22 -0500

    for refactored loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -51,19 +51,35 @@ public static void main(String[] args) {
     256
     65536*/
 
-    long i = 2;
-    do{
+//    long i = 2;
+//    do{
+////        System.out.println(i);
+////        i +=2;
+//
+////        System.out.println(i);
+////        i -= 5;
+//
+////        System.out.println(i);
+////        i*=i;
+//
+//    }while(i<=1000000);
+
+    /*Refactor the previous two exercises to use a for loop instead.*/
+
+//    for (int i = 5; i <= 15; i++){
 //        System.out.println(i);
-//        i +=2;
+//    }
 
+//    for (int i=0; i<=100;i+=2){
 //        System.out.println(i);
-//        i -= 5;
+//    }
 
-        System.out.println(i);
-        i*=i;
+//    for(int i=100; i>=-10; i-=5){
+//        System.out.println(i);
+//    }
 
-    }while(i<=1000000);
-
-
+//    for(long i=2; i<=1000000;i*=i){
+//        System.out.println(i);
+//    }
 }
 }
