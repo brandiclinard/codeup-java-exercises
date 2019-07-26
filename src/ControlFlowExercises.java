20432d8c8cc31b775079d36ffb8c721aac3ed02b
Brandi <brandiclinard021911@gmail.com>
2019-07-25 19:46:02 -0500

    bonus complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -15,11 +15,11 @@ public class ControlFlowExercises {
 
         5 6 7 8 9 10 11 12 13 14 15*/
 
-    //    int i = 5;
-    //    while( i<=15){
-    //        System.out.println(i);
-    //        i++;
-    //    }
+        //    int i = 5;
+        //    while( i<=15){
+        //        System.out.println(i);
+        //        i++;
+        //    }
 
         /*Do While
 
@@ -32,36 +32,37 @@ public class ControlFlowExercises {
         256
         65536*/
 
-    //    long i = 2;
-    //    do{
-    ////        System.out.println(i);
-    ////        i +=2;
-    //
-    ////        System.out.println(i);
-    ////        i -= 5;
-    //
-    ////        System.out.println(i);
-    ////        i*=i;
-    //
-    //    }while(i<=1000000);
+        //    long i = 2;
+        //    do{
+        ////        System.out.println(i);
+        ////        i +=2;
+        //
+        ////        System.out.println(i);
+        ////        i -= 5;
+        //
+        ////        System.out.println(i);
+        ////        i*=i;
+        //
+        //    }while(i<=1000000);
 
         /*Refactor the previous two exercises to use a for loop instead.*/
 
-    //    for (int i = 5; i <= 15; i++){
-    //        System.out.println(i);
-    //    }
+        //    for (int i = 5; i <= 15; i++){
+        //        System.out.println(i);
+        //    }
 
-    //    for (int i=0; i<=100;i+=2){
-    //        System.out.println(i);
-    //    }
+        //    for (int i=0; i<=100;i+=2){
+        //        System.out.println(i);
+        //    }
 
-    //    for(int i=100; i>=-10; i-=5){
-    //        System.out.println(i);
-    //    }
+        //    for(int i=100; i>=-10; i-=5){
+        //        System.out.println(i);
+        //    }
 
-    //    for(long i=2; i<=1000000;i*=i){
-    //        System.out.println(i);
-    //    }
+//        for(long i=2; i<=1000000; i*=i) {
+//            System.out.println(i);
+//        }
+        //    }
 
            /* One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
 
@@ -69,17 +70,17 @@ public class ControlFlowExercises {
         For multiples of three: print “Fizz” instead of the number.
         For the multiples of five: print “Buzz”.
         For numbers which are multiples of both three and five: print “FizzBuzz”.*/
-        for (int i = 1; i <= 100; i++){
-            if (i % 15 == 0) {
-                System.out.println("fizzbuzz");
-            } else if (i % 5 == 0){
-                System.out.println("buzz");
-            }else if (i % 3 == 0){
-                System.out.println("fizz");
-            }else{
-                System.out.println(i);
-            }
-        }
+//        for (int i = 1; i <= 100; i++){
+//            if (i % 15 == 0) {
+//                System.out.println("fizzbuzz");
+//            } else if (i % 5 == 0){
+//                System.out.println("buzz");
+//            }else if (i % 3 == 0){
+//                System.out.println("fizz");
+//            }else{
+//                System.out.println(i);
+//            }
+//        }
 
         /*Display a table of powers.
 
@@ -104,19 +105,89 @@ public class ControlFlowExercises {
 
          */
         Scanner scanner = new Scanner(System.in);
+//
+//        String choice = "y";
+//
+//        do {
+//            System.out.println("Please enter an integer:");
+//            int number = scanner.nextInt();
+//
+//            String header = "number | squared | cubed " + "\n"
+//                    + "------- | -------- | -------";
+//            System.out.println(header);
+//
+//
+//            int squaredNumber = 0;
+//            int cubedNumber = 0;
+//
+//            String row = "";
+//            for (int i = 0; i <= number; i++) {
+//
+//                squaredNumber = i * i;
+//                cubedNumber = i * i * i;
+//
+//                row = i + "        | " + squaredNumber + "       | " + cubedNumber;
+//                System.out.println(row);
+//            }
+//            System.out.println("Do you wish to continue? (y/n): ");
+//            choice = scanner.next();
+//            System.out.println();
+//
+//        }while(!choice.equalsIgnoreCase("n"));
 
-        System.out.println("Please enter an integer:");
-        String number = scanner.next();
-        Integer resultNumber = Integer.valueOf(number);
-        int squaredNumber = i *= i;
-        int cubedNumber = i = i^3;
+        /*Convert given number grades into letter grades.
+                -Prompt the user for a numerical grade from 0 to 100.
+        Display the corresponding letter grade.
+                -Prompt the user to continue.
+        Assume that the user will enter valid integers for the grades.
+        The application should only continue if the user agrees to.
+        Grade Ranges:
 
+        A : 100 - 88
+        B : 87 - 80
+        C : 79 - 67
+        D : 66 - 60
+        F : 59 - 0*/
 
-        System.out.println("number   |   squared    |   cubed   ");
-        System.out.println("--------- |  ----------- | ---------");
-        System.out.println(resultNumber + "        | "+squaredNumber+"           | "+cubedNumber+"        ");
+        String choice = "y";
 
+         do {
+             System.out.println("Let's convert your numerical grade to a letter grade. Please insert your numerical grade: ");
+             int numerical = scanner.nextInt();
 
+             if (numerical >= 97 && numerical <= 100) {
+                 System.out.println("A+");
+             }else if(numerical >=91 && numerical <=96){
+                 System.out.println("A");
+             }else if(numerical >=88 && numerical <=90) {
+                 System.out.println("A-");
+             }else if(numerical >= 86 && numerical <=87){
+                 System.out.println("B+");
+             }else if(numerical >=82 && numerical <=85){
+                 System.out.println("B");
+             } else if (numerical >= 80 && numerical <= 81) {
+                 System.out.println("B-");
+             }else if(numerical >= 76 && numerical <=79){
+                 System.out.println("C+");
+             }else if(numerical >= 71 && numerical <= 75){
+                 System.out.println("C");
+             }else if(numerical >= 67 && numerical <= 70) {
+                 System.out.println("C-");
+             }else if(numerical >= 65 && numerical <=66){
+                 System.out.println("D+");
+             }else if(numerical >=62 && numerical <=64) {
+                 System.out.println("D");
+             } else if (numerical >= 60 && numerical <= 66) {
+                 System.out.println("D-");
+             } else if (numerical >= 0 && numerical <= 59) {
+                 System.out.println("F");
+             }else {
+                 System.out.println("You did not enter numerical grade between 100 - 0.");
+             }
+             System.out.println("Do you wish to continue? (y/n): ");
+            choice = scanner.next();
+            System.out.println();
 
+         }while(!choice.equalsIgnoreCase("n"));
     }
 }
